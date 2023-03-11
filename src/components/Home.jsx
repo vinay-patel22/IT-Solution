4619f7fa62874a8c5d15faf0f72513f0e0a9ebb6
Vinay Patel <vinaypatel5519@gmai.com>
2023-03-11 21:32:31 +0530

    commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>You'r Time matter 
+            
+          </h1>
+          <p>Solution to all your problems .</p>
+        </main>
+      </div>
+
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            every day. We are leading tech company whose aim is to increase the
+            problem solving ability in children.
+          </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+          At our IT solution company, we pride ourselves on being a team of experienced and dedicated professionals who are passionate about technology and innovation. We understand that technology is constantly evolving, and we strive to stay ahead of the curve by continuously learning and adapting to new trends and developments. Our team is made up of experts in various fields, including software development, cybersecurity, data analytics, and cloud computing. We believe that our diverse range of skills and expertise allows us to provide comprehensive solutions to our clients. Our primary goal is to help businesses leverage technology to streamline their operations, enhance their productivity, and ultimately achieve their strategic objectives.
+           </p>
+        </div>
+      </div>
+
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
